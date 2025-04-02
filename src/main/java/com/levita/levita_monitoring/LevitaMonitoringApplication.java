ed3b7099722bd9b55dacd9ad85693c760b860ca7
Aleksey Cherepanov <mackvej@gmail.com>
2025-04-03 01:27:54 +0300

    Initial commit

---
+++ src/main/java/com/levita/levita_monitoring/LevitaMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.levita.levita_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevitaMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevitaMonitoringApplication.class, args);
+	}
+
+}
