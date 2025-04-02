68851c55a137c8a806548c024664b9c3f53d4dd6
Aleksey Cherepanov <mackvej@gmail.com>
2025-04-03 01:55:53 +0300

    added user entity

---
+++ src/main/java/com/levita/levita_monitoring/model/User.java
@@ -0,0 +1,29 @@
+package com.levita.levita_monitoring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+//    TODO: Шифрование
+    @Column(nullable = false)
+    private String password;
+
+}
