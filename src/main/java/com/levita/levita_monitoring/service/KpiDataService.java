03c8738802e3a69cb20ac2ee33ed640155b74522
Aleksey Cherepanov <mackvej@gmail.com>
2025-04-09 14:08:04 +0300

    Add handling method for main salary part

--- src/main/java/com/levita/levita_monitoring/service/KpiDataService.java
+++ src/main/java/com/levita/levita_monitoring/service/KpiDataService.java
@@ -8,6 +8,7 @@ import com.levita.levita_monitoring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
@@ -22,11 +23,35 @@ public class KpiDataService {
         this.userRepository = userRepository;
     }
 
+    private void handleMainSalaryPart(int userId, String value) {
+        try{
+            BigDecimal mainSalaryPart = new BigDecimal(value);
+
+            Optional<User> optUser = userRepository.findById((long) userId);
+            if (optUser.isEmpty()) {
+                System.out.printf("Пользователь с id %d не найден", userId);
+                return;
+            }
+            User user = optUser.get();
+            Optional<UserKpi> optKpi = userKpiRepository.findByUser(user);
+            UserKpi userKpi = optKpi.orElseGet( () -> {
+                UserKpi newKpi = new UserKpi();
+                newKpi.setUser(user);
+                return newKpi;
+            });
+            userKpi.setMainSalaryPart(mainSalaryPart);
+            userKpiRepository.save(userKpi);
+            System.out.printf("Сохранен mainSalaryPart для пользователя с id %d: %s", userId, value);
+        } catch (NumberFormatException e){
+            System.err.printf("Неверный формат для MAIN_SALARY_PART_%d: %s", userId, value);
+        }
+    }
+
     private void handleConversionRate(int userId, String value){
         try{
             Double conversion = Double.valueOf(value);
 
-            Optional<User> optUser = userRepository.findById(userId);
+            Optional<User> optUser = userRepository.findById((long) userId);
             if(optUser.isEmpty()){
                 System.out.printf("Пользователь с индексом %s не найден", userId);
                 return;
