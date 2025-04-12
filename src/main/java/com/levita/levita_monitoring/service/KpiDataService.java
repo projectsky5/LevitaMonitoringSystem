7ee8f34a6ac32a088f6d99488c842691d7228bb8
Aleksey Cherepanov <mackvej@gmail.com>
2025-04-12 17:57:18 +0300

    Add method for extracting name and location from table cell

--- src/main/java/com/levita/levita_monitoring/service/KpiDataService.java
+++ src/main/java/com/levita/levita_monitoring/service/KpiDataService.java
@@ -86,6 +86,17 @@ public class KpiDataService {
         };
     }
 
+    private String[] extractNameAndLocation(String raw){
+        String cleaned = raw.trim();
+        String[] parts = cleaned.split("\\s*\\(|\\)");
+
+        if(parts.length == 2){
+            return new String[]{parts[0].trim(), parts[1].trim()};
+        } else {
+            return new String[]{cleaned, ""};
+        }
+    }
+
     private void handleRemainingToPlan(int locationId, String value) {
         try {
             BigDecimal remainingToPlan = new BigDecimal(sanitizeNumericString(value));
