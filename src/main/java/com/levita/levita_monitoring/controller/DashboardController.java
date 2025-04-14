e3ae7d5743eecd39cf62ba3d98efe5aa6c032fb0
Aleksey Cherepanov <mackvej@gmail.com>
2025-04-15 00:00:27 +0300

    Add: DashboardContoller to return user KPI

---
+++ src/main/java/com/levita/levita_monitoring/controller/DashboardController.java
@@ -0,0 +1,25 @@
+package com.levita.levita_monitoring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    public DashboardController(DashboardService dashboardService){
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping
+    public ResponseEntity<DashboardDto> getDashboard(Principal principal){
+        DashboardDto dto = dashboardService.getDashboardForUser(principal.getName());
+        return ResponseEntity.ok(dto);
+    }
+}
