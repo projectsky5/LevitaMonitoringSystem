5ade46077203d070292b058f31a408d3d8f8ae7f
Aleksey Cherepanov <mackvej@gmail.com>
2025-04-16 14:12:38 +0300

    Add: dashboard controller to support fetching by user ID
    - Added endpoint GET /api/dashboard/id={id}
    - Allows retrieving dashboard data for nay user by ID

--- src/main/java/com/levita/levita_monitoring/controller/DashboardController.java
+++ src/main/java/com/levita/levita_monitoring/controller/DashboardController.java
@@ -4,6 +4,7 @@ import com.levita.levita_monitoring.dto.DashboardDto;
 import com.levita.levita_monitoring.service.DashboardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,15 @@ public class DashboardController {
         this.dashboardService = dashboardService;
     }
 
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<DashboardDto> getDashboard(){
         DashboardDto dto = dashboardService.getDashboardForUser("user_женя_варварская");
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/id={id}")
+    public ResponseEntity<DashboardDto> getDashboardById(@PathVariable Long id){
+        DashboardDto dto = dashboardService.getDashboardByUserId(id);
         return ResponseEntity.ok(dto);
     }
 }
