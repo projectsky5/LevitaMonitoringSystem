d00994d9f9591ee9dd218668015aaf98b8d2f6d5
projectsky5 <mackvej@gmail.com>
2025-05-03 23:58:05 +0300

    Refactor: update SheetsReportService with dynamic range updates and scheduled date insertion
    - Reworked updateShiftReport and updateCurrentReport to write into dynamic rows based on to today's date
    - Replaced append logic with update to prevent duplication and optimize API usage
    - Introduced Scheduled method to insert today's date into date columns at 3 AM
    - Integrated nextColumn method logic for range calculation after date columns
    - Refactored to fetch column definitions from YAML configs using key-based access
    - Ensured compatibility with new date-columns and current-columns structure

--- src/main/java/com/levita/levita_monitoring/service/SheetsReportService.java
+++ src/main/java/com/levita/levita_monitoring/service/SheetsReportService.java
@@ -3,6 +3,7 @@ package com.levita.levita_monitoring.service;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import com.levita.levita_monitoring.configuration.CurrentColumnsConfig;
+import com.levita.levita_monitoring.configuration.DateColumnsConfig;
 import com.levita.levita_monitoring.configuration.SheetNamesConfig;
 import com.levita.levita_monitoring.configuration.ShiftColumnsConfig;
 import com.levita.levita_monitoring.dto.reportDto.FullReportDto;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Service
 public class SheetsReportService {
@@ -23,50 +25,95 @@ public class SheetsReportService {
     private final ShiftColumnsConfig shiftColumnsConfig;
     private final SheetNamesConfig sheetNamesConfig;
     private final CurrentColumnsConfig currentColumnsConfig;
+    private final DateColumnsConfig dateColumnsConfig;
 
     @Value("${google.sheets.spreadsheetId}")
     private String spreadsheetId;
 
     private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM - E", Locale.forLanguageTag("ru-RU"));
 
-    public SheetsReportService(Sheets sheets, ShiftColumnsConfig shiftColumnsConfig, SheetNamesConfig sheetNamesConfig, CurrentColumnsConfig currentColumnsConfig) {
+    public SheetsReportService(Sheets sheets, ShiftColumnsConfig shiftColumnsConfig, SheetNamesConfig sheetNamesConfig, CurrentColumnsConfig currentColumnsConfig, DateColumnsConfig dateColumnsConfig) {
         this.sheets = sheets;
         this.shiftColumnsConfig = shiftColumnsConfig;
         this.sheetNamesConfig = sheetNamesConfig;
         this.currentColumnsConfig = currentColumnsConfig;
+        this.dateColumnsConfig = dateColumnsConfig;
+    }
+
+    public void insertDateRowIfMissing() throws IOException {
+        String today = LocalDate.now().format(dateFormatter);
+
+        for(Map.Entry<String, String> entry : dateColumnsConfig.getColumns().entrySet()){
+            String sheetName = entry.getKey();
+            String col = entry.getValue();
+
+            int startRow = switch (sheetName){
+                case "Пробные (команда Кати)", "Пробные (команда Алины)" -> 8;
+                case "Текущие (команда Кати)", "Текущие (команда Алины)" -> 6;
+                default -> 3;
+            };
+
+            String range = String.format("'%s'!%s%d:%s", sheetName, col, startRow, col);
+
+            ValueRange response = sheets.spreadsheets().values()
+                    .get(spreadsheetId, range)
+                    .execute();
+
+            List<List<Object>> values = response.getValues();
+            if (values != null) {
+                boolean alreadyExists = values.stream()
+                        .anyMatch(row -> !row.isEmpty() && today.equals(row.get(0).toString()));
+
+                if(!alreadyExists) {
+                    int rowToInsert = startRow + values.size();
+                    String insertRange = String.format("'%s'!%s%d", sheetName, col, rowToInsert);
+
+                    ValueRange body = new ValueRange().setValues(List.of(List.of(today)));
+
+                    sheets.spreadsheets().values()
+                            .update(spreadsheetId, insertRange, body)
+                            .setValueInputOption("USER-ENTERED")
+                            .execute();
+                }
+            }
+        }
+
     }
 
     public void appendFullReport(FullReportDto dto, User user) throws IOException {
         String today = LocalDate.now().format(dateFormatter);
 
-        appendShiftReport(dto.shift(), user, today);
+        updateShiftReport(dto.shift(), user, today);
 //        appendTrialReport(dto.trial(), user, today);
-        appendCurrentReport(dto.current(), user, today);
+        updateCurrentReport(dto.current(), user, today);
 //        appendOperationsReport(dto.operations(), user, today);
     }
 
-    public void appendShiftReport(FullReportDto.ShiftReportDto dto, User user, String today) throws IOException {
+    public void updateShiftReport(FullReportDto.ShiftReportDto dto, User user, String today) throws IOException {
         String location = user.getLocation().getName();
         String admin = user.getName();
 
-        String startCol = shiftColumnsConfig.getColumns().get(location);
-        if (startCol == null) {
+        String dateCol = shiftColumnsConfig.getColumns().get(location);
+        if (dateCol == null) {
             throw new IllegalArgumentException("Неизвестная локация: " + location);
         }
 
-        List<Object> row = List.of(
-                today,
+        int row = findRowByDate(sheetNamesConfig.getShift(), dateCol, 3 , today);
+
+        String startCol = nextColumn(dateCol);
+
+        List<Object> rowData = List.of(
                 dto.shiftStart(),
                 dto.shiftEnd(),
                 admin
         );
 
         String sheetName = sheetNamesConfig.getShift();
-        String range = sheetName + "!" + startCol + ":";
-        appendRow(row, range);
+        String range = String.format("'%s'!%s%d:", sheetName, startCol, row);
+        updateRow(rowData, range);
     }
 
-    public void appendCurrentReport(FullReportDto.CurrentReportDto dto, User user, String today) throws IOException {
+    public void updateCurrentReport(FullReportDto.CurrentReportDto dto, User user, String today) throws IOException {
         String admin = user.getName();
         String location = user.getLocation().getName();
 
@@ -76,15 +123,20 @@ public class SheetsReportService {
         };
 
         // Ключ для поиска в конфиге
-        String key = admin + " (" + location + ")";
-        List<String> columns = currentColumnsConfig.getColumns().get(key);
+        String key = String.format("%s (%s)", admin, location);
+        String rangeDef = currentColumnsConfig.getColumns().get(key);
 
-        if(columns == null){
-            throw new IllegalArgumentException("Не найдены координаты ячеек для пользователя: " + location);
+        if(rangeDef == null || !rangeDef.contains(":")) {
+            throw new IllegalArgumentException("Неверный диапазон колонок для пользователя: " + key);
         }
 
-        List<Object> row = List.of(
-                today,
+        String[] colRange = rangeDef.split(":");
+        String startCol = colRange[0];
+        String endCol = colRange[1];
+
+        int row = findRowByDate(sheetName, "A", 6, today);
+
+        List<Object> rowData = List.of(
                 dto.finished(),
                 dto.extended(),
                 dto.extendedAmount(),
@@ -102,16 +154,49 @@ public class SheetsReportService {
                 dto.singleVisitAmount()
         );
 
-        String range = sheetName + "!" + columns.getFirst() + ":";
-        appendRow(row, range);
+        String range = String.format("'%s'!%s%d:%s%d", sheetName, startCol, row, endCol, row);
+        updateRow(rowData, range);
     }
 
-    private void appendRow(List<Object> row, String range) throws IOException {
+    private void updateRow(List<Object> row, String range) throws IOException {
         ValueRange body = new ValueRange().setValues(List.of(row));
 
         sheets.spreadsheets().values()
-                .append(spreadsheetId, range, body)
+                .update(spreadsheetId, range, body)
                 .setValueInputOption("USER-ENTERED")
                 .execute();
+    }
+
+    private int findRowByDate(String sheetName, String column, int startRow, String date) throws IOException {
+        String range = String.format("'%s'!%s%d:%s", sheetName, column, startRow, column);
+        ValueRange response = sheets.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+
+        List<List<Object>> rows = response.getValues();
+        if (rows == null) return -1;
+
+        for (int relativeRowOffset = 0; relativeRowOffset < rows.size(); relativeRowOffset++) {
+            if (!rows.get(relativeRowOffset).isEmpty() && date.equals(rows.get(relativeRowOffset).get(0).toString())) {
+                return startRow + relativeRowOffset;
+            }
+        }
+        throw new IllegalArgumentException("Дата не найдена: " + date);
+    }
+
+    private String nextColumn(String column){
+        int length = column.length();
+        char[] chars = column.toUpperCase().toCharArray();
+
+        for (int i = length - 1; i >= 0; i--) {
+            if (chars[i] != 'Z') {
+                chars[i]++;
+                return new String (chars, 0, i + 1);
+            } else {
+                chars[i] = 'A';
+            }
+        }
+
+        return "A" + new String(chars);
     }
 }
